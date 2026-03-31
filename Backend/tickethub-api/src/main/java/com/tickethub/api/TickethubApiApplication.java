985ebb262ad6a5d80f03b354389e82c96d4336b5
PhungVanLong <phungvanlongbn04@gmail.com>
2026-03-31 10:42:18 +0700

    update api-gateway-v2

--- Backend/tickethub-api/src/main/java/com/tickethub/api/TickethubApiApplication.java
+++ Backend/tickethub-api/src/main/java/com/tickethub/api/TickethubApiApplication.java
@@ -3,8 +3,14 @@ package com.tickethub.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    FlywayAutoConfiguration.class,
+    KafkaAutoConfiguration.class
+})
 public class TickethubApiApplication {
 
 	public static void main(String[] args) {
